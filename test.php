9a5543497e6867229ae21a9cb7dae021842ae990
Daudi mabula <aw1116sm@gmail.com>
2025-09-17 18:55:10 +0300

    added lib and chatbot ai

--- test.php
+++ test.php
@@ -1,3 +1,7 @@
 <?php
-$str="Habari? mimi nimepata dharura kwahiyo sitaweza kufika nyumbani leo ";
-if
+require_once "vendor/autoload.php";
+$str="hl?\.";
+if(\Solobea\Helpers\data\Sanitizer::is_valid_message($str)){
+    echo "sms is valid";
+}
+else echo "invalid";
